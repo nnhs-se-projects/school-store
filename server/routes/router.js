96e0c010c546bdc08dee8734a7eb88980ef2ce5f
JamesCarsello <jjcarsellO@stu.naperville203.org>
2025-01-23 10:27:27 -0600

    homepage/adminpage flexbox for footer made in CSS

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,9 +4,6 @@ const Entry = require("../model/entry");
 const User = require("../model/user");
 const Item = require("../model/item");
 
-// easy way to assign static data (e.g., array of strings) to a variable
-const habitsOfMind = require("../model/habitsOfMind.json");
-
 // pass a path (e.g., "/") and a callback function to the get method
 //  when the client makes an HTTP GET request to the specified path,
 //  the callback function is executed
@@ -48,31 +45,6 @@ route.get("/", async (req, res) => {
 
 route.get("/admin", async (req, res) => {
   res.render("admin");
-});
-
-route.get("/createEntry", (req, res) => {
-  res.render("createEntry", { habits: habitsOfMind });
-});
-
-route.post("/createEntry", async (req, res) => {
-  const entry = new Entry({
-    // When the time zone offset is absent, date-only forms are interpreted as
-    //  a UTC time and date-time forms are interpreted as a local time. We want
-    //  the date object to reflect local time; so add a time of midnight.
-    date: new Date(req.body.date + "T00:00:00"),
-    email: req.session.email,
-    habit: req.body.habit,
-    content: req.body.content,
-  });
-  await entry.save();
-
-  res.status(201).end();
-});
-
-route.get("/editEntry/:id", async (req, res) => {
-  const entry = await Entry.findById(req.params.id);
-  console.log(entry);
-  res.send(entry);
 });
 
 route.post("/cart", async (req, res) => {
