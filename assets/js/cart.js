da70baf98d72d42d213d98226bca525dbb526013
krneill <krneill@stu.naperville203.org>
2025-02-20 10:57:37 -0600

    working on removing items from cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,7 +1,30 @@
 const warnUserInput = document.getElementById("warning");
+const removeButton = document.querySelector("button.remove-item");
+const quantityButton = document.querySelector("button.update-quantity");
+const googleId = document.getElementById("googleId").value;
+
 if (warnUserInput.value === "true") {
   // If the warning input is present, display the alert
   const message =
     "Warning: One or more items in your cart are out of stock and have been removed. Please check your cart for details.";
   alert(message);
 }
+
+removeButton.addEventListener("click", async () => {
+  const itemId = removeButton.getAttribute("data-id");
+  const response = await fetch("/cart/remove", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      googleId,
+      itemId,
+    }),
+  });
+  if (response.ok) {
+    window.location.reload();
+  } else {
+    console.error("Error removing item from cart");
+  }
+});
