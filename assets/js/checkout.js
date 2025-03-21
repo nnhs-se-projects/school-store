432de00a34b15d2758afb6d8f1cdd6155f558dd9
krneill <krneill@stu.naperville203.org>
2025-03-21 09:53:47 -0500

    Update checkout request body to include pickUpDate, pickUpPeriod, and totalCost

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ googleId }),
+      body: JSON.stringify({ googleId, pickUpDate, pickUpPeriod, totalCost }),
     });
 
     if (response.ok) {
