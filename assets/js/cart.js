79fa23f78172c0700cfeaee80301ec337c4dfadb
JamesCarsello <jjcarsellO@stu.naperville203.org>
2025-04-14 10:29:42 -0500

    Update nodemailer to version 6.10.1 in package.json and package-lock.json; remove unused quantitySelectors in cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const warnUserOOSInput = document.getElementById("warningOOS");
   const warnUserQuantInput = document.getElementById("warningQuant");
 
-  const quantitySelectors = document.querySelectorAll(
-    "select.quantity-dropdown"
-  );
   const checkoutButton = document.getElementById("checkout-button");
   const googleId = document.getElementById("googleId").value;
 
@@ -24,7 +21,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   checkoutButton.addEventListener("click", async (event) => {
     event.preventDefault(); // Prevent the default form submission
-    if (quantitySelectors.length === 0) {
+    const quantitySelected = parseInt(
+      document.querySelector(".quantity-selected").textContent.trim(),
+      10
+    );
+
+    if (quantitySelected.length === 0) {
       alert(
         "Your cart is empty. Please add items to your cart before checking out."
       );
@@ -45,39 +47,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     } else {
       console.error("Failed to initiate checkout");
     }
-  });
-
-  console.log(quantitySelectors);
-  quantitySelectors.forEach((button) => {
-    // Perform actions on each button
-    button.addEventListener("change", async (event) => {
-      const quantity = event.target.value;
-      const index = button.getAttribute("data-index");
-      if (quantity === "0") {
-        const confirmRemoval = confirm(
-          "Are you sure you want to remove this item from the cart?"
-        );
-        if (!confirmRemoval) {
-          event.target.value = button.getAttribute("data-original-quantity");
-          return;
-        }
-      }
-
-      const response = await fetch("/cart/updateQuant", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ googleId, index, quantity }),
-      });
-
-      if (response.ok) {
-        console.log("Item updated in cart");
-        window.location.reload();
-      } else {
-        console.error("Failed to update item in cart");
-      }
-    });
   });
 
   const decreaseButtons = document.querySelectorAll(
