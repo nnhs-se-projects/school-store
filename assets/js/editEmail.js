d0b5de80df9404a2f3c209825e565d33706e16b5
JKSquires <168765098+JKSquires@users.noreply.github.com>
2026-03-04 10:14:28 -0600

    Save email text in database
    Co-authored-by: Andrey Kopal <aakopal@stu.naperville203.org>

--- assets/js/editEmail.js
+++ assets/js/editEmail.js
@@ -1,9 +1,17 @@
 const saveBtn = document.getElementById("save-btn");
 
+function setIfEmpty(text) {
+  if (text) {
+    return text;
+  } else {
+    return "\n";
+  }
+}
+
 saveBtn.addEventListener("click", async () => {
-  const confirmStoreText = document.getElementById("confirm-store-text").value;
-  const confirmStudentText = document.getElementById("confirm-student-text").value;
-  const cancelStudentText = document.getElementById("cancel-student-text").value;
+  const confirmStoreText = setIfEmpty(document.getElementById("confirm-store-text").value);
+  const confirmStudentText = setIfEmpty(document.getElementById("confirm-student-text").value);
+  const cancelStudentText = setIfEmpty(document.getElementById("cancel-student-text").value);
 
   const emailText = {
     confirmStoreText,
