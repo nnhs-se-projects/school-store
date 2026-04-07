6abfab49588e4ad734856f6a8a3e0c14ea643c11
Jason Squires <168765098+JKSquires@users.noreply.github.com>
2026-04-07 08:43:48 -0500

    Improve site navigation (#61)
    * Add button to jump to store part of the home page
    
    * Stick navbar to screen top on desktop for easy navigation
    
    * update button appear delay to 2.5 sec
    
    ---------
    
    Co-authored-by: Andrey Kopal <aakopal@stu.naperville203.org>

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,4 +1,27 @@
-/**
- * contains client-side Javascript functions
- *  (primarily event handlers to fetch data from the Node server)
- */
+const notLoggedInItems = document.querySelectorAll("#not-logged-in");
+const notLoggedInAsStudentItems = document.querySelectorAll(
+  "#not-logged-in-as-student",
+);
+
+const jumpToStore = document.getElementById("jump-to-store");
+
+notLoggedInItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    alert("Please log in to view item details.");
+  });
+});
+
+notLoggedInAsStudentItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    alert("Please log in as a student to view item details.");
+  });
+});
+
+jumpToStore.addEventListener("click", () => {
+  window.location.href = "#item-list-skip";
+});
+
+setTimeout(() => {
+  jumpToStore.style.opacity = 1;
+  console.log("button showing", jumpToStore.style.display);
+}, 2500);
